7d4beb9c54507f969e2b65a57b292b1993c1b888
shihuang <gkshi@foxmail.com>
2021-07-19 16:06:52 +0800

    code: 增加注释

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 // import './index.css'
+// 由于单独的 css 文件引入需要通过客户端模块(源码的 https://github.com/vitejs/vite/blob/1.x/src/client/client.ts)来更新样式，故没有实现
 
 createApp(App).mount('#app')
